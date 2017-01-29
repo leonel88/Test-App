3b9527e8fa0da701a9d247a500f4f6fae7392c52
Leonardo Giannì <leonardogianni@Leonardos-iMac.lan>
2017-01-29 15:54:48 -0800

    Made a little change

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Hello World!")
+        print("Hello Leonardo!")
     }
 
     override func didReceiveMemoryWarning() {
